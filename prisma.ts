1decb4ab0141d6e27b833d31a829d4019192ec05
Terresa Pan <terresap2010@gmail.com>
2024-04-17 14:17:48 -0700

    change prisma.ts file

--- prisma.ts
+++ prisma.ts
@@ -1,15 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+
 declare global {
-    var prisma: PrismaClient
-}
-if(process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient()
-}
-else {
-    if(!global.prisma) {
-        global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma
+	var prisma: PrismaClient | undefined;
 }
 
-export default prisma
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export default prisma;
