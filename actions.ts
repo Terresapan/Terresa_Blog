876ebb2813077af476a100ecce452c15355f17ac
Terresa Pan <terresap2010@gmail.com>
2024-04-17 14:00:53 -0700

    add Vercel Postgress

--- actions.ts
+++ actions.ts
@@ -1,26 +1,14 @@
 'use server';
-import sql from 'better-sqlite3';
 import { redirect } from 'next/navigation';
+import prisma  from './prisma';
 
-const db = sql('./users.db');
-
-async function saveSubscription(subscription: { name: string; email: string }) {
-  const insert = db.prepare('INSERT INTO users (name, email) VALUES (?, ?)');
-  insert.run(subscription.name, subscription.email);
-}
-
-export async function handleSubscription(formData: FormData) {
+export async function handleSubscription(data: FormData) {
   const subscription = {
-    name: formData.get('name') as string,
-    email: formData.get('email') as string,
+    name: data.get('name') as string,
+    email: data.get('email') as string,
   };
 
-  await saveSubscription(subscription);
-
-   // Clear the form fields
-   for (const [name, _] of formData) {
-    formData.set(name, '');
-  }
+  await prisma.user.create({ data: subscription });
 
   redirect('/');
 } 
